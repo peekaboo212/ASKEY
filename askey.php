490ddb92d88e609f76d00f819b31fb0f59105d7c
JonathanValdes98 <78063564+JonathanValdes98@users.noreply.github.com>
2021-11-25 15:45:57 -0500

    Add files via upload

--- askey.php
+++ askey.php
@@ -1,4 +1,4 @@
-<?php include 'templates/header.php'?>
+<?php include 'templates/header.php'?> 
 
 <?php
 include_once "model/conexion.php";
@@ -7,92 +7,92 @@ $ProArticulo= $sentencia->fetchAll(PDO::FETCH_OBJ);
 //print_r($ProArticulo);
 
 ?>
+<!-- alerta-->
+<?php
+            include 'alerts.php';
+            ?>
+            <!-- alerta-->
 
-<div class="container-fluid mt-4 ">
-    <div class="row">
-    <div class="col-md-3">
-            <div class="card">
-                <div class="card-header">
-                    Materia
-                </div>
-                <form  class="p-4" method="POST" action="registrar.php">
-                <select class="form-select" aria-label="Default select example">
-  <option selected>Open this select menu</option>
-  <option value="1">One</option>
-  <option value="2">Two</option>
-  <option value="3">Three</option>
-</select>
-                    <div class="mb-3">
-                        <label class="form-label">Nombre</label>
-                        <input type="text" class="form-control" name="nombre" autofocus autocomplete="off"  >
+<div class="container mt-4 ">
+    <div class="row justify-content-between">
 
-                    </div>
-                
-                    <div class="mb-3">
-                        <label class="form-label">Descripcion</label>
-                        <input type="text" class="form-control" name="descripcion" autofocus autocomplete="off" >
-                        
-                    </div>
-                    <div class="mb-3">
-                        <label class="form-label">Precio</label>
-                        <input type="text" class="form-control" name="price" autofocus autocomplete="off"  >
-                        
-                    </div>
+        <!-- Seccion filtros -->
+        <div class="col-3 mt-lg-4 p-lg-4">
+            <div class="container">
+                <form  class="" method="POST" action="registrar.php">
+                <label class="form-label">Filtra tus preguntas</label>
+                    <select class="form-select mb-2" aria-label="Default select example">
+                        <option selected>Materia</option>
+                        <option value="1">Bases de datos</option>
+                        <option value="2">Diseño de interfaces</option>
+                        <option value="3">Electricidad y magnetismo</option>
+                        <option value="4">Hab cogn y creativ</option>
+                        <option value="1">Inglés IV</option>
+                        <option value="1">Mat ingeniería I</option>
+                        <option value="1">Programación web</option>
+                    </select>
+                    <select class="form-select mb-2" aria-label="Default select example">
+                        <option selected>Mostrar</option>
+                        <option value="1">Preguntas respondidas</option>
+                        <option value="2">Preguntas sin responder</option>
+                    </select>
+                    <select class="form-select mb-2" aria-label="Default select example">
+                        <option selected>Ordenar por</option>
+                        <option value="1">Más populares</option>
+                        <option value="2">Recientes</option>
+                    </select>
                     <div class="d-grid">
                         <input type="hidden" name="oculto" value="1">
-                        <input type="submit" class="btn btn-primary" value="Ingresar">
+                        <input type="submit" class="btn btn-outline-primary" value="Ingresar">
                     </div>
                 </form>
-
             </div>
         </div>
-        <div class="col-md-9">
-            <!-- alerta-->
-            <?php 
-                if(isset($_GET['mensaje']) and $_GET['mensaje'] == 'nocompleted'){
-            ?>
-            <div class="alert alert-danger alert-dismissible fade show" role="alert">
-                <strong>¡Olvidaste Algo!</strong> Rellena todos los campos.
-                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                </div>
-             <?php 
-            }?>
-            <?php 
-                if(isset($_GET['mensaje']) and $_GET['mensaje'] == 'completo'){
-            ?>
-            <div class="alert alert-success alert-dismissible fade show" role="alert">
-                <strong>¡Correcto!</strong> Registro completado.
-                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                </div>
-             <?php 
-            }?>
-
-            <?php 
-                if(isset($_GET['mensaje']) and $_GET['mensaje'] == 'actualizado'){
-            ?>
-            <div class="alert alert-success alert-dismissible fade show" role="alert">
-                <strong>¡Correcto!</strong> Registro actualizado.
-                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                </div>
-             <?php 
-            }?>
-
-            <?php 
-                if(isset($_GET['mensaje']) and $_GET['mensaje'] == 'error'){
-            ?>
-            <div class="alert alert-danger alert-dismissible fade show" role="alert">
-                <strong>¡Error!</strong> Intente de nuevo.
-                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                </div>
-             <?php 
-            }?>
-            <!-- alerta-->
+        <!-- /Seccion filtros -->
+        
+        <div class="col-12 col-lg-8 me-lg-5 ms-lg-5">
+        
+            
             <div class="card">
+                <!-- Elementos del encabezado -->
+                <div class="card-header">
+                    <div class="row">
+                        <div class="col-4 me-auto">
+                            Pregunta.
+                            @nombre
+                        </div>
+                    </div>
+                </div>
+                <!-- /Elementos del encabezado -->
+                
+                <!-- Realizar publicacion -->
+                <div class="col-12 col-lg-9 me-lg-5 ms-lg-5 m-2">
+                   
+                        <?php include 'config.php' ?>
+                        <form method="post">
+                            <div class="form-floating mb-3">
+                                <textarea class="form-control" rows="3" placeholder="" id="floatingTextarea" name="publicacion"></textarea>
+                                <label for="floatingTextarea">Haz tu pregunta</label>
+                            </div>
+                            <div class="md-3">
+                                <input type="hidden" name="oculto" value="1">
+                                <input type="submit" class="btn btn-primary " value="Publicar" name="publicar">
+                            </div>
+                        </form>
+                        <?php include("publicacion.php"); ?>
+                    
+                </div>
+                <!-- /Realizar publicacion -->
+            </div>
+
+            <!-- Seccion mostrar preguntas -->
+            <div class="card mt-4">
                 <div class="card-header">
                     Pregunta.
                 </div>
+                
                 <div class="p-2">
-                    <table class="table aling-middle">
+                    <table class="table aling-middle p-2">
                         <thead>
                             <tr>
                                 <th scope="col"> nombre</th>
@@ -111,11 +111,13 @@ $ProArticulo= $sentencia->fetchAll(PDO::FETCH_OBJ);
                                 <td><?php echo $dato->precioArticulo; ?></td>
                                 <td ><a class="text-success" href="editar.php?codigo=<?php echo $dato->idArticulo; ?>"><i class="bi bi-pencil-square"></a></i></td>
                                 <td class="text-danger"><a href=""><i class="bi bi-trash"></a></i></td>
+
                                 
                             </tr>
                             <?php
                             }
                             ?>
+
                             
                         </tbody>
                     </table>
@@ -150,6 +152,7 @@ $ProArticulo= $sentencia->fetchAll(PDO::FETCH_OBJ);
 
                 </div>
             </div>
+            <!-- /Seccion mostrar preguntas -->
         </div>
        
     </div>
